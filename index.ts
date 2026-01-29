7970aaadcdff592ddcd2b79f3cf6ba8bfb826f01
Michał Warda <t1du53k@gmail.com>
2026-01-29 12:40:05 +0100

    Introduce plugin

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import type { Plugin } from "@opencode-ai/plugin";
+
+const OpencodeArchitect: Plugin = async (input) => {
+  return {
+    config: async (config) => {
+      config.agent = config.agent || {};
+      config.agent["test"] = {
+        description: "test",
+        name: "test",
+        model: undefined,
+        temperature: undefined,
+        prompt: "You are a test agent.",
+      };
+      return;
+    },
+  };
+};
+
+export default OpencodeArchitect;
